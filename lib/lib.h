87f686fa77b8c26f7d8dbca2d3c653801434fdb2
Sabelo <sabelo.sethu@yahoo.com>
2017-06-01 00:36:14 -0700

    Return array of words in a line created by a delimeter

--- lib/lib.h
+++ lib/lib.h
@@ -1,7 +1,10 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 #define HASHSIZE 101
+#define LINESIZE 1000
+#define MAXLINEWORDS 20
 
 typedef struct user { 
   char *key;
@@ -77,7 +80,7 @@ short manhattan_distance(rating *r1, rating *r2) {
   short score = 0;
   while ( r1 != NULL && r1->key != NULL && r2 != NULL && r2->key != NULL) 
     if (strcmp(r1->key, r2->key) == 0) 
-      score += abs((r1++)->score - (r2++)->score);
+      score += fabs((r1++)->score - (r2++)->score);
 
   return score;
 }
@@ -95,4 +98,63 @@ float euclidean_distance(rating *r1, rating *r2) {
 void error_creating (char*name) {
   printf("Error creating %s\n", name);
   exit(1);
+}
+
+void usage(char *progname) {
+  printf("Usage: %s <ratings csv file> \n", progname);
+  exit(0);
+}
+
+int get_num_lines(char *filename) {
+  FILE *fp;
+  char line[LINESIZE];
+  int count = 0;
+  if ((fp = fopen(filename, "r")) != NULL) 
+    while (fgets(line, LINESIZE, fp) != NULL)
+      count++;  
+  return count;
+}
+
+/* Returns an Array of words. Remember to Free Array after use */
+char ** delim(char *str, char delim) {
+  int num_words = 0;
+  char char_counter = 0; // num of chars in word
+  char wordsize[MAXLINEWORDS];
+  char c;
+  int pos = 0;
+  int str_ptr_offset = 0;
+  // Determine how many delimetered words there are
+  while (++str_ptr_offset && (c = *str++) != '\0' && c != '\n' && ++char_counter <= MAXLINEWORDS) {
+        if (c == delim || *str == '\0' || *str == '\n') {
+          if (c == delim)             
+            char_counter = char_counter - 1;   // don't count delimeter as a letter
+	  num_words++;
+	  wordsize[pos++] = char_counter;
+	  char_counter = 0; 
+	}
+  }
+  str -= str_ptr_offset;
+  // allocate words of words
+  char **words = malloc(num_words * sizeof(char *));
+  // allocate for each word in list
+  for (pos = 0; pos < num_words; pos++)
+	*words++ = malloc(wordsize[pos] * sizeof(char *));
+  words -= pos; // *words temporarily goes to dangerous territory array overbound. On a separate note resetting so word can start from first letter	
+  char_counter = 0;
+  while ((c = *str++) != '\0' && c != '\n') {
+    if (c == delim) {
+      **words = '\0'; // complete word creation
+      *words = *words - char_counter;
+      char_counter = 0;
+      words++; // next word 
+    }
+	else {
+	  (*(*words)++) = c; // **words = 'c' THEN *words++. 
+          char_counter++;
+        }
+  }
+  **words = '\0'; // End last word which has no delimeter after it
+  *words = *words - char_counter;
+  words -= pos-1; // Note: this time *word didn't go off by 1 but pos is still off by 1, so subtract 1 to error correct
+  return words;
 }
